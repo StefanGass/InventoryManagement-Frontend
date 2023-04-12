a19d68b461df1959ddc9e2bfad319e9025084f78
Martin Pressl <99990993+martpres@users.noreply.github.com>
2023-04-12 22:31:02 +0200

    camera upload - basic function

--- src/components/image-upload/ImageUpload.tsx
+++ src/components/image-upload/ImageUpload.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FC, useContext, useEffect, useState } from 'react';
+import {ChangeEvent, FC, useContext, useEffect, useRef, useState} from 'react';
 import { Box, Button, Grid, Typography } from '@mui/material';
 import { Delete, UploadFile } from '@mui/icons-material';
 import { IPicture, IPictureUpload } from 'components/interfaces';
@@ -13,6 +13,13 @@ interface IImageUploadProps {
 const ImageUpload: FC<IImageUploadProps> = ({ setPictures, disabled }) => {
     const [pictureList, setPictureList] = useState<IPictureUpload[]>([]);
     const { themeMode } = useContext(UserContext);
+    const [value, setValue] = useState('');
+    const [error, setError] = useState(false);
+    const [scan, setScan] = useState(false);
+    const [scanError, setScanError] = useState(false);
+    const [placeholderIcon, setPlaceholderIcon] = useState(true);
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const qrVideo = useRef<HTMLVideoElement>(null);
 
     useEffect(() => {
         const transformedList = pictureList?.map((pic) => {
@@ -25,7 +32,6 @@ const ImageUpload: FC<IImageUploadProps> = ({ setPictures, disabled }) => {
 
     const transformPictures = async (changeEvent: ChangeEvent<HTMLInputElement>) => {
         if (changeEvent.target.files) {
-
             const filePromises = Array.from(changeEvent.target.files).map((file) => {
                 return new Promise((resolve, reject) => {
                     const reader = new FileReader();
@@ -43,6 +49,76 @@ const ImageUpload: FC<IImageUploadProps> = ({ setPictures, disabled }) => {
                 changeEvent.target.value = '';
                 setPictureList(pictureList.concat(res as IPictureUpload[]));
             });
+        }
+    };
+    const makePicture = (boo) => {
+        try{
+            if (canvasRef != null && canvasRef.current != null && qrVideo.current != null) {
+                const canvas = canvasRef?.current?.getContext('2d');
+                if(canvas !=null){
+                    canvas.drawImage(qrVideo?.current, 0, 0, canvasRef.current.width, canvasRef.current?.height);
+                    const imageData = canvas.getImageData(0, 0, canvasRef?.current?.width, canvasRef?.current?.height);
+                    console.log(imageData);
+                    const bild = canvasRef.current.toDataURL();
+                    console.log(canvasRef.current.toDataURL())
+                    var image = new Image();
+                    image.src = bild;
+                    setPictureList(pictureList.concat(({ name: "test", base64: canvasRef.current.toDataURL()})));
+                }
+            }
+        }catch(error){
+           console.log(error);
+        }
+    };
+    const startCamera = (props: boolean) => {
+        setScanError(false);
+        setValue('');
+        if (navigator?.mediaDevices) {
+            if (qrVideo?.current) {
+                navigator.mediaDevices
+                    .getUserMedia({ video: { facingMode: 'environment' } })
+                    .then(function (stream) {
+                        if (qrVideo.current !== null) {
+                            setPlaceholderIcon(false);
+                            qrVideo.current.srcObject = stream;
+                            qrVideo.current.setAttribute('playsinline', 'playsinline'); // required to tell iOS safari we don't want fullscreen
+                            if (props) {
+                                qrVideo.current.scrollIntoView({
+                                    behavior: 'smooth',
+                                    block: 'center',
+                                    inline: 'nearest'
+                                });
+                                qrVideo.current.play();
+                            } else {
+                                qrVideo.current.pause();
+                                qrVideo.current.currentTime = 0;
+                            }
+                        } else {
+                            console.log('Video stream is null.');
+                            setScan(false);
+                            setPlaceholderIcon(true);
+                            setScanError(true);
+                        }
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                        setScan(false);
+                        setPlaceholderIcon(true);
+                        setScanError(true);
+                        console.log()
+                    });
+            }
+        } else {
+            console.log('No camera device found.');
+            setScan(false);
+            setPlaceholderIcon(true);
+            setScanError(true);
+            setError(true);
+            console.log(value);
+            console.log(error);
+            console.log(scan);
+            console.log(scanError);
+            console.log(placeholderIcon);
         }
     };
 
@@ -75,6 +151,48 @@ const ImageUpload: FC<IImageUploadProps> = ({ setPictures, disabled }) => {
                         onChange={(changeEvent) => transformPictures(changeEvent)}
                     />
                 </Button>
+
+                <Button
+                    onClick={(changeEvent) => startCamera(true)}
+                    variant="contained"
+                    component="label"
+                    color="secondary"
+                    sx={{
+                        marginLeft: '10px',
+                        width: '19.35em',
+                        height: '4em',
+                        border: `${!disabled ? (themeMode === 'dark' ? '1px solid' + darkGrey : '1px solid' + lightGrey) : null}`,
+                        '&:hover': {
+                            color: `${themeMode === 'dark' ? mainWhite : mainBlack}`,
+                            backgroundColor: 'initial',
+                            border: `1px solid ${themeMode === 'dark' ? mainWhite : mainBlack}`
+                        }
+                    }}
+                    disabled={disabled}
+                >
+                    <Typography>&nbsp;&nbsp;Kamera starten&nbsp;&nbsp;&nbsp;</Typography>
+
+                </Button>
+                <div style={{display: 'flex'}}>
+                    <video
+                        ref={qrVideo}
+                        width="300px"
+                        height="300px"
+                        style={{ objectFit: 'cover' , marginTop: '50px', display: 'block' }}
+                    />
+
+                    <canvas style={{marginRight: '10px'}}
+                        /* Necessary for reading QR code from scanner */
+                        id="canvas"
+                        width="300px"
+                        height="300px"
+                        ref={canvasRef}
+                    />
+                    <Button onClick={(changeEvent) => makePicture(true)} style={{marginRight: '10px'}} >
+                        Foto aufnehmen
+                    </Button>
+                </div>
+
             </Grid>
             <Box sx={{ marginTop: '10px', width: '17.5em' }}>
                 {pictureList?.map((pic, index) => (
