a70bbefe38e8e6c480b4e63ed7a56125ad4672df
Lukas Proksch <2120859001@fh-burgenland.at>
2023-04-24 20:18:30 +0200

    [IM-12] playwright tests

--- testsE2E/loginPage.spec.ts
+++ testsE2E/loginPage.spec.ts
@@ -1,5 +1,7 @@
 import { expect, test } from "@playwright/test";
-import { NavigationHelper } from "./helper/navigationHelper";
+import { NavigationHelper, NavigationPage } from "./helper/navigationHelper";
+
+const base64 = require('base-64');
 
 test.describe("LoginPage", () => {
 
@@ -17,5 +19,22 @@ test.describe("LoginPage", () => {
     test("has 'Abmelden' button after succesful login", async ({page}) => {
         await NavigationHelper.login(page);
         await expect(page.locator("#abmeldenButton")).toBeVisible()
+    });
+
+    test("set cookie 'rememberMe' after login with 'Angemeldet Bleiben'", async ({page}) => {
+        const username = "Super Admin";
+        await NavigationHelper.start(page);
+        await page.locator("#username").fill(username);
+        await page.locator("#password").fill("password");
+        await page.click("#angemeldetBleiben");
+        await page.click("#loginButton");
+        await NavigationHelper.assertPageLoaded(page, NavigationPage.dashboard);
+        expect((await page.context().cookies()).map(c => c.name)).toContain("rememberMe");
+        expect((await page.context().cookies()).map(c => c.value)).toContain(base64.encode(username.replace(" ", "_")));
+    });
+
+    test("don't set cookie 'rememberMe' after login without 'Angemeldet Bleiben'", async ({page}) => {
+        await NavigationHelper.login(page);
+        expect((await page.context().cookies()).length).toEqual(0);
     });
 });
