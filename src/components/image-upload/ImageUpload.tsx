2eaf2fdf4aeebca4228d6c627e24a5ee5538aa4b
Martin Pressl <99990993+martpres@users.noreply.github.com>
2023-05-11 15:15:07 +0200

    adapt video and picture format, improve styling

--- src/components/image-upload/ImageUpload.tsx
+++ src/components/image-upload/ImageUpload.tsx
@@ -1,10 +1,12 @@
-import {ChangeEvent, FC, useContext, useEffect, useRef, useState} from 'react';
-import { Box, Button, Grid, Typography } from '@mui/material';
-import {CameraAlt, Delete, UploadFile} from '@mui/icons-material';
+import { ChangeEvent, FC, useContext, useEffect, useRef, useState } from 'react';
+import { Box, Button, Container, Grid, Tooltip, Typography, useMediaQuery } from '@mui/material';
+import { CameraAlt, Delete, PhotoCamera, UploadFile } from '@mui/icons-material';
 import { IPicture, IPictureUpload } from 'components/interfaces';
-import { darkGrey, lightGrey, mainBlack, mainWhite } from 'styles/theme';
+import lightTheme, { darkGrey, lightGrey, mainBlack, mainWhite } from 'styles/theme';
 import { UserContext } from 'pages/_app';
-import { format } from 'date-fns'
+import { format } from 'date-fns';
+import styles from 'styles/ImageUpload.module.scss';
+import CustomButton from 'components/form-fields/CustomButton';
 
 interface IImageUploadProps {
     setPictures: (pictures: IPicture[]) => void;
@@ -12,25 +14,16 @@ interface IImageUploadProps {
 }
 
 const ImageUpload: FC<IImageUploadProps> = ({ setPictures, disabled }) => {
-    const [pictureList, setPictureList] = useState<IPictureUpload[]>([]);
     const { themeMode } = useContext(UserContext);
-    const [value, setValue] = useState('');
-    const [error, setError] = useState(false);
-    const [scan, setScan] = useState(false);
-    const [scanError, setScanError] = useState(false);
-    const [placeholderIcon, setPlaceholderIcon] = useState(true);
     const canvasRef = useRef<HTMLCanvasElement>(null);
-    const qrVideo = useRef<HTMLVideoElement>(null);
-    const [camera, setCamera] = useState(false);
+    const cameraVideo = useRef<HTMLVideoElement>(null);
+    const [pictureList, setPictureList] = useState<IPictureUpload[]>([]);
+    const [noCameraAvailableError, setNoCameraAvailableError] = useState(false);
+    const [isShowPlaceholderIcon, setIsShowPlaceholderIcon] = useState(true);
+    const [isCameraActive, setIsCameraActive] = useState(false);
+    const [cameraSettings, setCameraSettings] = useState<MediaTrackSettings | null>(null);
 
-    useEffect(() => {
-        const transformedList = pictureList?.map((pic) => {
-            return { id: undefined, pictureUrl: pic.base64 };
-        });
-        if (transformedList) {
-            setPictures(transformedList);
-        }
-    }, [pictureList]);
+    const matches = useMediaQuery(lightTheme.breakpoints.down('md'));
 
     const transformPictures = async (changeEvent: ChangeEvent<HTMLInputElement>) => {
         if (changeEvent.target.files) {
@@ -53,91 +46,104 @@ const ImageUpload: FC<IImageUploadProps> = ({ setPictures, disabled }) => {
             });
         }
     };
-    const makePicture = (boo) => {
-        try{
-            if (canvasRef != null && canvasRef.current != null && qrVideo.current != null) {
+
+    const takePicture = () => {
+        try {
+            if (canvasRef != null && canvasRef.current != null && cameraVideo.current != null) {
                 const canvas = canvasRef?.current?.getContext('2d');
-                if(canvas !=null){
-                    canvas.drawImage(qrVideo?.current, 0, 0, qrVideo.current.width, qrVideo.current?.height);
-                    const imageData = canvas.getImageData(0, 0, qrVideo?.current?.width, qrVideo?.current?.height);
-                    console.log(imageData);
-                    const bild = canvasRef.current.toDataURL();
-                    console.log(canvasRef.current.toDataURL())
+                if (canvas != null) {
+                    canvas.drawImage(cameraVideo.current, 0, 0, cameraVideo.current.width, cameraVideo.current.height);
                     var image = new Image();
-                    image.src = bild;
-                    var name = "cameraUpload_" + format(new Date(), 'dd-MM-yyyy_HH-mm-ss')
-                    setPictureList(pictureList.concat(({ name: name, base64: canvasRef.current.toDataURL()})));
+                    image.src = canvasRef.current.toDataURL();
+                    var name = 'cameraUpload_' + format(new Date(), 'dd-MM-yyyy_HH-mm-ss');
+                    setPictureList(pictureList.concat({ name: name, base64: canvasRef.current.toDataURL() }));
                 }
             }
-        }catch(error){
-           console.log(error);
+        } catch (error) {
+            console.log(error);
         }
     };
+
     const startCamera = (visibile: boolean) => {
         try {
-            setCamera(visibile);
-            setScanError(false);
-            setValue('');
+            setIsCameraActive(visibile);
+            setNoCameraAvailableError(false);
             if (navigator?.mediaDevices) {
-                if (qrVideo?.current) {
+                if (cameraVideo?.current) {
                     navigator.mediaDevices
-                        .getUserMedia({ video: { facingMode: 'environment' } })
+                        .getUserMedia({
+                            video: {
+                                facingMode: 'environment',
+                                width: { ideal: matches ? 1080 : 2160 },
+                                height: { ideal: matches ? 2160 : 1080 }
+                            }
+                        })
                         .then(function (stream) {
-                            if (qrVideo.current !== null) {
-                                setPlaceholderIcon(false);
-
-                                qrVideo.current.srcObject = stream;
-                                qrVideo.current.setAttribute('playsinline', 'playsinline'); // required to tell iOS safari we don't want fullscreen
+                            if (cameraVideo.current !== null) {
+                                setIsShowPlaceholderIcon(false);
+                                setCameraSettings(stream.getVideoTracks()[0].getSettings());
+                                cameraVideo.current.srcObject = stream;
+                                cameraVideo.current.setAttribute('playsinline', 'playsinline'); // required to tell iOS safari we don't want fullscreen
                                 if (visibile) {
-                                    qrVideo.current.scrollIntoView({
+                                    cameraVideo.current.scrollIntoView({
                                         behavior: 'smooth',
                                         block: 'center',
                                         inline: 'nearest'
                                     });
-
-                                    qrVideo.current.play();
-
+                                    cameraVideo.current.play();
                                 } else {
-                                    qrVideo.current.srcObject.getTracks().forEach(track => track.stop()) ;
-                                    qrVideo.current.srcObject.getAudioTracks().forEach(track => track.stop()) ;
-                                    qrVideo.current.srcObject.getVideoTracks().forEach(track => track.stop()) ;
-                                    qrVideo.current.pause();
-
+                                    cameraVideo.current.srcObject.getTracks().forEach((track) => track.stop());
+                                    cameraVideo.current.srcObject.getAudioTracks().forEach((track) => track.stop());
+                                    cameraVideo.current.srcObject.getVideoTracks().forEach((track) => track.stop());
+                                    cameraVideo.current.pause();
                                 }
                             } else {
                                 console.log('Video stream is null.');
-                                setScan(false);
-                                setPlaceholderIcon(true);
-                                setScanError(true);
+                                setIsShowPlaceholderIcon(true);
+                                setNoCameraAvailableError(true);
                             }
                         })
                         .catch((error) => {
                             console.log(error);
-                            setScan(false);
-                            setPlaceholderIcon(true);
-                            setScanError(true);
-                            console.log()
+                            setIsShowPlaceholderIcon(true);
+                            setNoCameraAvailableError(true);
                         });
                 }
             } else {
-                console.log('No camera device found.');
-                setScan(false);
-                setPlaceholderIcon(true);
-                setScanError(true);
-                setError(true);
-                console.log(value);
-                console.log(error);
-                console.log(scan);
-                console.log(scanError);
-                console.log(placeholderIcon);
+                setIsShowPlaceholderIcon(true);
+                setNoCameraAvailableError(true);
             }
-        }catch (error){
+        } catch (error) {
+            console.log(error);
+            setIsShowPlaceholderIcon(true);
+            setNoCameraAvailableError(true);
         }
     };
 
+    useEffect(() => {
+        const transformedList = pictureList?.map((pic) => {
+            return { id: undefined, pictureUrl: pic.base64 };
+        });
+        if (transformedList) {
+            setPictures(transformedList);
+        }
+    }, [pictureList]);
+
+    useEffect(() => {
+        startCamera(false);
+    }, [matches]);
+
     return (
-        <Box sx={{ display: 'flex', flexFlow: 'column wrap', width: '100%', alignItems: 'center' }}>
-            <Grid sx={{ cursor: `${disabled ? 'not-allowed' : 'pointer'}` }}>
+        <Container maxWidth="lg">
+            <Grid
+                container
+                justifyContent="center"
+                alignItems="center"
+                direction="row"
+                sx={{
+                    cursor: `${disabled ? 'not-allowed' : 'pointer'}`
+                }}
+            >
                 <Button
                     variant="contained"
                     component="label"
@@ -145,6 +151,7 @@ const ImageUpload: FC<IImageUploadProps> = ({ setPictures, disabled }) => {
                     sx={{
                         width: '19.35em',
                         height: '4em',
+                        margin: '0.5em',
                         border: `${!disabled ? (themeMode === 'dark' ? '1px solid' + darkGrey : '1px solid' + lightGrey) : null}`,
                         '&:hover': {
                             color: `${themeMode === 'dark' ? mainWhite : mainBlack}`,
@@ -164,76 +171,95 @@ const ImageUpload: FC<IImageUploadProps> = ({ setPictures, disabled }) => {
                         onChange={(changeEvent) => transformPictures(changeEvent)}
                     />
                 </Button>
-
-                <Button
-                    variant="contained"
-                    component="label"
-                    color="secondary"
-                    //ref={kameraEinButton}
-                    onClick={() => startCamera(!camera)}
-                    sx={{
-                        marginLeft: '10px',
-                        width: '19.35em',
-                        height: '4em',
-                        border: `${!disabled ? (themeMode === 'dark' ? '1px solid' + darkGrey : '1px solid' + lightGrey) : null}`,
-                        '&:hover': {
-                            color: `${themeMode === 'dark' ? mainWhite : mainBlack}`,
-                            backgroundColor: 'initial',
-                            border: `1px solid ${themeMode === 'dark' ? mainWhite : mainBlack}`
-                        }
-                    }}
-                    disabled={disabled}
-                >
-                    <CameraAlt />
-                    <Typography>&nbsp;&nbsp;Kamera {camera?'stoppen':'starten'} &nbsp;&nbsp;&nbsp;</Typography>
-                </Button>
-
-
-
-                    <div style={{display: camera? 'flex': 'none'}}>
-                        <video
-                            ref={qrVideo}
-                            width="500px"
-                            height="375px"
-                            style={{  marginTop: '50px', display: 'block' }}
-                        />
-
-                        <canvas style={{marginRight: '10px',marginTop:'50px', marginLeft: '10px'}}
-                            /* Necessary for reading QR code from scanner */
-                                id="canvas"
-                                width="500px"
-                                height="375px"
-                                ref={canvasRef}
-                        />
+                <Tooltip title={noCameraAvailableError ? 'Kamera nicht verfügbar.' : null}>
+                    <div style={{ cursor: `${noCameraAvailableError ? 'not-allowed' : 'pointer'}` }} /* necessary to show tooltip */>
+                        <Button
+                            variant="contained"
+                            component="label"
+                            color="secondary"
+                            onClick={() => startCamera(!isCameraActive)}
+                            sx={{
+                                width: '19.35em',
+                                height: '4em',
+                                margin: '0.5em',
+                                border: `${!disabled ? (themeMode === 'dark' ? '1px solid' + darkGrey : '1px solid' + lightGrey) : null}`,
+                                '&:hover': {
+                                    color: `${themeMode === 'dark' ? mainWhite : mainBlack}`,
+                                    backgroundColor: 'initial',
+                                    border: `1px solid ${themeMode === 'dark' ? mainWhite : mainBlack}`
+                                }
+                            }}
+                            disabled={noCameraAvailableError}
+                        >
+                            <CameraAlt />
+                            <Typography>&nbsp;&nbsp;Kamera {isCameraActive ? 'stoppen' : 'starten'} &nbsp;&nbsp;&nbsp;</Typography>
+                        </Button>
                     </div>
-
-
-
-            <div style={{display: camera? '': 'none'}}>
-                <Button
-                    onClick={() => makePicture(true)}
-                    style={{width:'100%'}}
-                >
-                    Foto aufnehmen
-                </Button>
-            </div>
-
+                </Tooltip>
             </Grid>
-            <Box sx={{ marginTop: '10px', width: '17.5em' }}>
-                {pictureList?.map((pic, index) => (
-                    <Box
-                        key={`${pic.name}-${index}`}
-                        sx={{ display: 'flex', flexFlow: 'row nowrap' }}
-                    >
-                        <Delete
-                            sx={{ cursor: 'pointer' }}
-                            onClick={() => setPictureList(pictureList?.filter((filterPic) => filterPic !== pic))}
-                        />
-                        <div>{pic.name}</div>
+            <Grid
+                container
+                justifyContent="center"
+                alignItems="center"
+                direction="row"
+                display={isCameraActive ? 'flex' : 'none'}
+            >
+                {isShowPlaceholderIcon && (
+                    <Box sx={{ m: 13.5 }}>
+                        <PhotoCamera style={{ color: 'LightGray', transform: 'scale(6)' }} />
                     </Box>
-                ))}
-            </Box>
-        </Box>
+                )}
+                <video
+                    ref={cameraVideo}
+                    width={cameraSettings ? cameraSettings.width : 0}
+                    height={cameraSettings ? cameraSettings.height : 0}
+                    className={styles.video}
+                    playsInline
+                    hidden={isShowPlaceholderIcon}
+                />
+                <canvas
+                    id="canvas"
+                    width={cameraSettings ? cameraSettings.width : 0}
+                    height={cameraSettings ? cameraSettings.height : 0}
+                    ref={canvasRef}
+                    className={styles.video}
+                    hidden={isShowPlaceholderIcon}
+                />
+            </Grid>
+            <Grid
+                container
+                justifyContent="center"
+                alignItems="center"
+                direction="row"
+                display={isCameraActive ? 'flex' : 'none'}
+            >
+                <CustomButton
+                    onClick={() => takePicture()}
+                    label="Foto aufnehmen"
+                />
+            </Grid>
+            <Grid
+                container
+                justifyContent="center"
+                alignItems="center"
+                direction="row"
+            >
+                <Box sx={{ marginTop: '10px', width: '17.5em' }}>
+                    {pictureList?.map((pic, index) => (
+                        <Box
+                            key={`${pic.name}-${index}`}
+                            sx={{ display: 'flex', flexFlow: 'row nowrap' }}
+                        >
+                            <Delete
+                                sx={{ cursor: 'pointer' }}
+                                onClick={() => setPictureList(pictureList?.filter((filterPic) => filterPic !== pic))}
+                            />
+                            <div>{pic.name}</div>
+                        </Box>
+                    ))}
+                </Box>
+            </Grid>
+        </Container>
     );
 };
 
